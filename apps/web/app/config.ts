8d9dddab65b8f0c965bd0670e5a636bebe8622b4
Akshaya Mohan <akshayamohan.2401@gmail.com>
2025-02-18 13:18:29 -0800

    Fixed Typescript Errors

--- apps/web/app/config.ts
+++ apps/web/app/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL="http://localhost:8080"
+export const BACKEND_URL="https://api.photo.akshayamohan.com"
 export const CLOUDFLARE_URL="https://pub-9ee50bda64c54532b15f88b2cf369491.r2.dev"
